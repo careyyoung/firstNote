b498b2f1f35780ea9b1a3c5b9a55bca00be90a21
382094632@qq.com <ye@carey-pc>
2015-03-01 00:28:34 +0800

    commit for update bat

--- python/django/updateDjangoConf.bat
+++ python/django/updateDjangoConf.bat
@@ -3,10 +3,30 @@ rem var1 backup folder
 rem var2 backuped folder
 rem var3 update folder
 
+choice /c whc /m "work_pc enter w, home_pc enter h, cancel enter c:"
+if errorlevel 3 goto cancel
+if errorlevel 2 goto home_pc
+if errorlevel 1 goto work_pc
+
+:home_pc
+echo.
+echo set home_pc
+set var1=D:\app\Python27\Lib\site-packages\django\conf
+set var2=D:\app\Python27\Lib\site-packages\django\conf_backup
+set var3=D:\softwares\gitHub\firstNote\python\django\django-conf
+echo set home_pc ok
+goto start
+
+:work_pc
+echo.
+echo set work_pc
 set var1=C:\app\Python27\Lib\site-packages\django\conf
 set var2=C:\app\Python27\Lib\site-packages\django\conf_backup
 set var3=E:\eclipse_git\firstNote\python\django\django-conf
+echo set work_pc ok
+goto start
 
+:start
 echo.
 echo.
 echo ### backup folder ###
@@ -44,6 +64,10 @@ goto exit
 ECHO success!!!  
 ECHO all done!!!
 goto exit
+
+:cancel
+echo.
+echo you cancel operate!
 
 :exit
 echo.
